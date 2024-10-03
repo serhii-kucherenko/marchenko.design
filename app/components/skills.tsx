b1841981050d44656c2a2eaf40c3fbb28883b6cc
Serhii Kucherenko <kucherenko.web@gmail.com>
2024-10-02 22:35:20 -0700

    update skills

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -1,17 +1,15 @@
-import { Typography, useTheme } from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 import { InfoSection } from "./info-section";
 
 export const Skills: React.FC = () => {
-  const theme = useTheme();
-
   return (
     <InfoSection title="Skills">
       <Typography variant="h5" fontWeight={600} pb={1} lineHeight="32px">
-        UI/UX Design expertise • Design Thinking • User Centric Design •
-        Usability Testing • Problem Solving • Project Management • Process
-        Improvement • Decision Making • Research&Analytics • Mentoring • Team
-        Collaboration • Natural Leadership
+        UI/UX design expertise • Design thinking • User-centric design •
+        Usability testing • Problem solving • Project management • Process
+        improvement • Decision making • Research & analytics • Mentoring • Team
+        collaboration • Natural leadership
       </Typography>
     </InfoSection>
   );
